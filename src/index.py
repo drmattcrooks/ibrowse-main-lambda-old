a6a45736ba1a160e00682b207afa3e8bc9b9e715
mattcrooks-tf <matthew.crooks@typeform.com>
2022-06-08 15:02:46 +0100

    first upload

---
+++ src/index.py
@@ -0,0 +1,12 @@
+import json
+import numpy as np
+
+print("loading lambda")
+
+
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'question': question,
+        'body': json.dumps({'Matt Says': f'A Random Number: {np.random.choice(range(1, 11))}'})
+    }
